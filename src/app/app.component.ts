ac46661091a44f432b5f7cfee84c99e37613ce7f
Vlaimir Korba <krissky1986@gmail.com>
2024-02-02 16:18:11 +0200

    1 - Components + styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,11 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  // templateUrl: './app.component.html',
+  // styleUrl: './app.component.scss',
+  template: ` <h1>My first Angular component</h1> `,
+  styles: ['h1 { color: papayawhip; font-size: 50px; text-align: center; }'],
 })
 export class AppComponent {
-  title = 'angular-crash-course-2022';
+  title = 'angular app';
 }
